37e051dfff7ed04c0135a78b8a9be7d0241c9181
Thorizar <ulucguler@hotmail.com>
2025-02-24 16:27:43 +0300

    main güncellendi.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,74 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Person person = new Person("John", "Doe", 20);
+        System.out.println("Firstname: " + person.getFirstName());
+        System.out.println("LastName: " + person.getLastName());
+        System.out.println("Age: " + person.getAge());
+
+        Wall wall = new Wall(5, 4);
+        System.out.println("area= " + wall.getArea());
+        wall.setHeight(-1.5);
+        System.out.println("width= " + wall.getWidth());
+        System.out.println("height= " + wall.getHeight());
+        System.out.println("area= " + wall.getArea());
+    }
+}
+
+class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isTeen() {
+        return age >= 13 && age <= 19;
+    }
+}
+
+class Wall {
+    private double width;
+    private double height;
+
+    public Wall(double width, double height) {
+        this.width = width < 0 ? 0 : width;
+        this.height = height < 0 ? 0 : height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setWidth(double width) {
+        this.width = width < 0 ? 0 : width;
+    }
+
+    public void setHeight(double height) {
+        this.height = height < 0 ? 1.0 : height;
+    }
+
+    public double getArea() {
+        return width * height;
     }
 }
